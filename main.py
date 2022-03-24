13445e08c2881ea33d1ebdf6ef51406cfa450853
theCrackbot <101562650+theCrackbot@users.noreply.github.com>
2022-03-25 01:29:22 +0530

    Initial commit US States Guessing Game

---
+++ main.py
@@ -0,0 +1,44 @@
+import turtle
+import pandas
+
+screen = turtle.Screen()
+screen.title("U.S States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+guessed_states = []
+
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
+                                    prompt="What's another state name").title()
+
+    if answer_state == "Exit":
+        missing_state = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_state.append(state)
+        break
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
+
+
+screen.exitonclick()
+
+#if answer_state is one of the states in all the states of 50_states.csv
+#   if they got it right:
+#       Create a turtle to write the name of the state at the state's x and y coordinate
+
+
+
+
+
